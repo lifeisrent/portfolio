e325d69fe9f6a69f2e43667d7f24e1f24b66d200
lifeisrent <smc9759@gmail.com>
2026-05-24 14:00:44 +0900

    content: move education graduation line into expandable details

--- data/projects.js
+++ data/projects.js
@@ -23,9 +23,10 @@ const profileData = {
       title: '학력',
       iconSvg: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"/></svg>',
       inlineHeadText: '고려대학교 (안암)',
-      lines: ['학부 4년 졸업'],
+      lines: [],
       expandable: true,
       details: [
+        '문과대학 학부 4년 졸업',
         '고려대학교 선택과목 수강 내역',
         'C언어 프로그래밍 및 알고리즘 설계 강의 이수',
         'JAVA 객체지향 프로그래밍 기반 애플리케이션 제작 강의 이수',
